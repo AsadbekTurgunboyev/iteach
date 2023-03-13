09b6acea40108063f63efdd121660d82693939c3
Jamshidbek202 <zamsidbekmamazonov@gmail.com>
2023-03-13 17:21:53 +0500

    Some changes.

--- app/src/main/java/com/example/iteach/avtivities/WarehouseActivity.java
+++ app/src/main/java/com/example/iteach/avtivities/WarehouseActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.iteach.HomeFragment.adapter.PersonAdapter;
 import com.example.iteach.HomeFragment.adapter.WarehouseAdapter;
 import com.example.iteach.R;
+import com.example.iteach.model.LoanModel;
 import com.example.iteach.model.PaymentReceiverModel;
 import com.example.iteach.model.Resource;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -92,50 +96,142 @@ public class WarehouseActivity extends AppCompatActivity {
         bottomSheetDialog.setContentView(R.layout.add_resource_bottom_sheet);
 
         TextInputLayout name = bottomSheetDialog.findViewById(R.id.edt_resource_name);
+        TextInputLayout store_name = bottomSheetDialog.findViewById(R.id.edt_store_name);
         TextInputLayout quantity = bottomSheetDialog.findViewById(R.id.edt_resource_quantity);
+        TextView overall_price = bottomSheetDialog.findViewById(R.id.txt_overall_price);
+        TextInputLayout price = bottomSheetDialog.findViewById(R.id.edt_resource_price);
+        TextInputLayout payment = bottomSheetDialog.findViewById(R.id.edt_payment);
         MaterialButton btn_add = bottomSheetDialog.findViewById(R.id.btn_add_resource);
+
+        String txt_overall_price = "";
+
+        if (price.getEditText().getText().toString() == ""){
+            Toast.makeText(WarehouseActivity.this, "Birinchi bir kiloga narxini kiriting!", Toast.LENGTH_SHORT).show();
+        } else {
+            quantity.getEditText().addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    overall_price.setText(String.valueOf(Integer.parseInt(quantity.getEditText().getText().toString()) * Integer.parseInt(price.getEditText().getText().toString())) + " so'm");
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+            });
+        }
 
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String txt_name = name.getEditText().getText().toString().trim();
+                String txt_store_name = store_name.getEditText().getText().toString().trim();
                 String txt_quantity = quantity.getEditText().getText().toString().trim();
+                String txt_price = price.getEditText().getText().toString().trim();
+                String txt_payment = payment.getEditText().getText().toString().trim();
 
-                if (txt_name != "" && txt_quantity != ""){
+                if (txt_name != "" && txt_store_name != "" && txt_quantity != "" && txt_price != "" && txt_payment != ""){
 
-                    FirebaseDatabase db = FirebaseDatabase.getInstance();
-                    DatabaseReference reference = db.getReference().child("Resources").child(txt_name);
+                    if (Integer.parseInt(txt_payment) > Integer.parseInt(txt_overall_price)){
+                        Toast.makeText(WarehouseActivity.this, "Ortiqcha pul tolandi!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        String loan = String.valueOf(Integer.parseInt(txt_price) - Integer.parseInt(txt_payment));
 
-                    reference.addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            Resource model = new Resource(txt_name, txt_quantity);
+                        if (Integer.parseInt(loan) == 0){
 
-                            reference.setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("money");
+
+                            reference.addListenerForSingleValueEvent(new ValueEventListener() {
                                 @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isComplete()){
-                                        Toast.makeText(WarehouseActivity.this, "Saqlandi!", Toast.LENGTH_SHORT).show();
-                                        bottomSheetDialog.dismiss();
-                                        recreate();
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    String money_there = snapshot.getValue().toString();
 
-                                    } else {
-                                        Toast.makeText(WarehouseActivity.this, "Xatolik!", Toast.LENGTH_SHORT).show();
-                                        bottomSheetDialog.dismiss();
-                                    }
+                                    String new_balance = String.valueOf(Integer.parseInt(money_there) - Integer.parseInt(txt_payment));
+
+                                    reference.setValue(new_balance).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+
+                                            DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference().child("Resources").child(txt_name);
+
+                                            Resource r_model = new Resource(txt_name, txt_store_name, txt_quantity, txt_price, txt_overall_price, txt_payment);
+                                            reference1.setValue(r_model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    Toast.makeText(WarehouseActivity.this, "Saqlandi!", Toast.LENGTH_SHORT).show();
+                                                    bottomSheetDialog.dismiss();
+                                                }
+                                            });
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                    Toast.makeText(WarehouseActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
                                 }
                             });
-                        }
 
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-                            Toast.makeText(WarehouseActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                        } else {
+
+                            DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("money");
+
+                            reference.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    String money_there = snapshot.getValue().toString();
+
+                                    String new_balance = String.valueOf(Integer.parseInt(money_there) - Integer.parseInt(txt_payment));
+
+                                    reference.setValue(new_balance);
+
+                                    DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference().child("Finance").child("MyLoans").child(txt_store_name);
+
+                                    LoanModel model = new LoanModel(txt_store_name, txt_price, txt_overall_price, txt_payment, loan);
+
+                                    reference1.setValue(model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isComplete()){
+
+                                                DatabaseReference reference2 = FirebaseDatabase.getInstance().getReference().child("Resources").child(txt_name);
+
+                                                Resource r_model = new Resource(txt_name, txt_store_name, txt_quantity, txt_price, txt_overall_price, txt_payment);
+                                                reference2.setValue(r_model).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<Void> task) {
+                                                        Toast.makeText(WarehouseActivity.this, "Saqlandi!", Toast.LENGTH_SHORT).show();
+                                                        bottomSheetDialog.dismiss();
+                                                    }
+                                                });
+
+                                            } else {
+                                                Toast.makeText(WarehouseActivity.this, "Xatolik!", Toast.LENGTH_SHORT).show();
+                                                bottomSheetDialog.dismiss();
+                                            }
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+                                    Toast.makeText(WarehouseActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                                    bottomSheetDialog.dismiss();
+                                }
+                            });
+
                         }
-                    });
+                    }
 
                 } else {
-                    Toast.makeText(WarehouseActivity.this, "Malumotlarni kiriting!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(WarehouseActivity.this, "Ma'lumotlarni kiriting!", Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
 
--- app/src/main/java/com/example/iteach/model/Resource.java
+++ app/src/main/java/com/example/iteach/model/Resource.java
@@ -3,14 +3,32 @@ package com.example.iteach.model;
 public class Resource {
 
     String name;
+
+    String store_name;
     String quantity;
+    String price;
+
+    String overall_price;
+    String payment;
 
     public Resource() {
     }
 
-    public Resource(String name, String quantity) {
+    public Resource(String name, String store_name, String quantity, String price, String overall_price, String payment) {
         this.name = name;
+        this.store_name = store_name;
         this.quantity = quantity;
+        this.price = price;
+        this.overall_price = overall_price;
+        this.payment = payment;
+    }
+
+    public String getOverall_price() {
+        return overall_price;
+    }
+
+    public void setOverall_price(String overall_price) {
+        this.overall_price = overall_price;
     }
 
     public String getName() {
@@ -29,5 +47,27 @@ public class Resource {
         this.quantity = quantity;
     }
 
+    public String getPrice() {
+        return price;
+    }
 
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getStore_name() {
+        return store_name;
+    }
+
+    public void setStore_name(String store_name) {
+        this.store_name = store_name;
+    }
+
+    public String getPayment() {
+        return payment;
+    }
+
+    public void setPayment(String payment) {
+        this.payment = payment;
+    }
 }
